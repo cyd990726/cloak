10ea9c4c55d1c8c332ea38991d31f1aadfe6a189
chenyandong <chenyandong@bytedance.com>
2026-07-26 13:28:40 +0800

    Add Vercel runtime ping
    Co-authored-by: TRAE CLI <noreply@bytedance.com>

---
+++ api/ping.go
@@ -0,0 +1,16 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func Ping(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Cache-Control", "no-store")
+	_ = json.NewEncoder(w).Encode(map[string]string{
+		"ping": "ok",
+		"time": time.Now().UTC().Format(time.RFC3339),
+	})
+}
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module cloak
 
-go 1.26.4
+go 1.26
 
 require (
 	github.com/oschwald/geoip2-golang v1.13.0 // indirect
